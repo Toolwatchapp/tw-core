4ce9652ba56531ae789eccc7d3185c17dfbb9086
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-06-14 14:16:08 -0400

    export modules

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,6 @@
 export * from './environment';
 export * from './api-client.component';
+export * from './domain/measure';
+export * from './domain/user';
+export * from './domain/watch';
+export * from './twapi.service';

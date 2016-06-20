6fa4e150c072a3862c752227ff67190b8b6c93e9
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-06-20 15:56:56 -0400

    add signup form

--- src/app/directives/signup/signup.component.ts
+++ src/app/directives/signup/signup.component.ts
@@ -1,50 +1,170 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 import {TRANSLATE_PROVIDERS, TranslateService, TranslatePipe, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
-import {  FormBuilder, Validators, Control, ControlGroup, FORM_DIRECTIVES, NgForm }    from '@angular/common';
+import {FORM_DIRECTIVES, FormBuilder, Control, ControlGroup, Validators}  from '@angular/common';
 import {TwAPIService} from './../../services/twapi.service'
 import {Http, HTTP_PROVIDERS, Headers}  from '@angular/http';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { GlobalValidator } from './../global-validator';
 
 @Component({
   selector: 'app-signup',
   templateUrl: 'app/directives/signup/signup.component.html',
   styleUrls: ['app/directives/signup/signup.component.css'],
   pipes: [TranslatePipe],
-  providers: [TwAPIService, HTTP_PROVIDERS]
+  providers: [TwAPIService, HTTP_PROVIDERS],
+  directives: [FORM_DIRECTIVES, MD_BUTTON_DIRECTIVES]
 })
+/**
+ * Signup form. Emits a userLogged event on new user signup
+ */
 export class SignupComponent implements OnInit {
 
-  email: string;
-  emailRepeat: string;
-  password: string;
-  passwordRepeat: string;
-  firstname: string;
-  lastname: string;
-  country: string;
-  success: boolean = true;
+  signupForm: ControlGroup;
+  email:Control;
+  emailRepeat:Control;
+  password: Control;
+  passwordRepeat: Control;
+  firstName: Control = new Control();
+  lastName: Control = new Control();
+  country: Control = new Control();
+  submitAttempt:boolean = false;
+  emailTaken:boolean = false;
+  error:boolean = false;
+  countries:[string];
+  filteredList = [];
+  query: string = "";
 
+  @Output() userLogged = new EventEmitter();
 
-  constructor(private translate: TranslateService,
-	  private twapi: TwAPIService) {
+   /**
+   * Constructor w/ service injection
+   * @param {TranslateService} private translate [description]
+   * @param {TwAPIService}     private twapi     [description]
+   * @param {FormBuilder}      private builder   [description]
+   */
+  constructor(private translate: TranslateService, 
+    private twapi: TwAPIService, private builder: FormBuilder) { 
 	  //use navigator lang if available
-	  var userLang = navigator.language.split('-')[0];
-	  userLang = /(fr|en)/gi.test(userLang) ? userLang : 'en';
-	  translate.setDefaultLang('en');
-	  translate.use(userLang);
+    //Lang definition
+    var userLang = navigator.language.split('-')[0];
+    userLang = /(fr|en)/gi.test(userLang) ? userLang : 'en';
+    translate.setDefaultLang('en');
+    translate.use(userLang);
+
+    translate.get("countries").subscribe((result: [string]) => {
+      this.countries = result;
+    });
+
+    //Form constraints
+    this.password = new Control('', Validators.required);
+    this.email = new Control(
+      '', 
+      Validators.compose(
+        [Validators.required, 
+        GlobalValidator.mailFormat]
+      )
+    );
+    this.emailRepeat = new Control(
+      '', 
+      Validators.compose(
+        [Validators.required, 
+        GlobalValidator.mailFormat]
+      )
+    );
+    this.password = new Control(
+      '',
+      Validators.compose(
+        [Validators.required,
+        Validators.minLength(8)]
+      )
+    );
+    this.passwordRepeat = new Control(
+      '',
+      Validators.compose(
+        [Validators.required,
+          Validators.minLength(8)]
+      )
+    );
+
+
+    this.signupForm = builder.group({
+        email: this.email,
+        password: this.password,
+        emailRepeat: this.emailRepeat,
+        passwordRepeat: this.passwordRepeat,
+        lastName: this.lastName,
+        firstName: this.firstName,
+        country: this.country
+      }, { 
+        validator: Validators.compose([
+          GlobalValidator.match(
+            "email",
+            "emailRepeat",
+            { "emailMatch": true }),
+          GlobalValidator.match(
+            "password",
+            "passwordRepeat",
+            { "passwordMatch": true }),
+          ]) 
+      }
+    );
   }
 
-  onSubmit(){
-	  console.log(this.email, this.password);
+  /**
+   * Trims the filteredList accoring to the 
+   * country field
+   * @param {string} query [description]
+   */
+  filter(query:string) {
+    this.filteredList = this.countries.filter(function(element:string){
+      return element.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    });
+  }
 
-	  this.twapi.signup(
-	  	this.email, 
-	  	this.password,
-	  	this.firstname, 
-	  	this.lastname, '').then(
-		  response => {
+  /**
+   * Assign the country label when selected 
+   * from the list
+   * @param {string} item [description]
+   */
+  select(item:string){
+    this.query = item;
+    this.filteredList = [];
+  }
 
-			  this.success = typeof response == "User";
-		  }
-	  )
+  /**
+   * submit the form
+   * @param {string    }} user [description]
+   */
+  onSubmit(user:{
+      email: string, 
+      password: string,
+      emailRepeat: string,
+      passwordRepeat: string,
+      lastName: string,
+      firstName: string
+    }) {
+
+    this.submitAttempt = true;
+
+    if(this.signupForm.valid){
+      this.twapi.signup(user.email,
+       user.password,
+       user.firstName,
+       user.lastName,
+       this.query).then(
+        res => { this.userLogged.emit(res) },
+        error => {
+          switch (error.status) {
+            case TwAPIService.HTTP_UNAUTHORIZED:
+              this.emailTaken = true;
+              break;
+            default:
+              this.error = true;
+              break;
+          }
+        }
+       )
+    }
   }
 
   ngOnInit() {

6a9281ccb001df6df8b67247a7851e8d11b208b4
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-10-11 07:27:48 -0400

    cleanup moon phases after rc0 & final

--- src/app/directives/clock/moon-phases/moon-phases.component.ts
+++ src/app/directives/clock/moon-phases/moon-phases.component.ts
@@ -3,23 +3,25 @@ import { Astro } from './../astro';
 
 @Component({
   selector: 'moon-phases',
-  template: ''
+  template: `<div class="moon-phase-container">
+               <div class="moon-mask"></div>
+               <div class="moon-disque"></div>
+             </div>
+             `
 })
 export class MoonPhasesComponent extends Astro implements OnInit {
   
   constructor(private elementRef: ElementRef) {
   	super();
-	this.illum = this.getMoonIllumination(new Date());
-	this.pos = this.getMoonPosition(new Date(), 0, 0);
-	this.angleMoon = this.illum.angle - this.pos.parallacticAngle;
-	console.log(this.illum.angle-this.pos.parallacticAngle);
-	console.log();
+	  this.illum = this.getMoonIllumination(new Date());
+	  this.pos = this.getMoonPosition(new Date(), 0, 0);
+	  this.angleMoon = this.illum.angle - this.pos.parallacticAngle;
   }
 
   ngAfterViewInit() {
-	let elem = this.elementRef.nativeElement.querySelector('.moon-disque')
-	elem.style.webkitTransform = 'rotateZ(' + this.angleMoon*360 + 'deg)';
-	elem.style.transform = 'rotateZ(' + this.angleMoon*360 + 'deg)';
+  	let elem = this.elementRef.nativeElement.querySelector('.moon-disque')
+  	elem.style.webkitTransform = 'rotateZ(' + this.angleMoon*360 + 'deg)';
+  	elem.style.transform = 'rotateZ(' + this.angleMoon*360 + 'deg)';
   }
 
   ngOnInit() {

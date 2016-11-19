65bdb9ff573482b3d8d145cd1ef7e8505c293647
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-11-19 18:44:07 -0500

    fix(analytics): Bring back ga service

--- services/ga.service.ts
+++ services/ga.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-//import {Analytics} from './analytics';
+// import { Analytics } from './analytics';
 declare let ga;
+declare let gaNewElem;
+declare let gaElems;
+declare let currdate;
 
 @Injectable()
 export class GAService {
@@ -9,37 +12,52 @@ export class GAService {
   public static appVersion:string;
   private static gaCreated:boolean = false;
 
+
+
   private static spawn(){
 
-      // if(!GAService.gaCreated){
-      //   ga('create', 'UA-59148878-1', 'auto');
-      //   GAService.gaCreated = true;
-      // }
+      if(!GAService.gaCreated){
+        /* tslint:disable:no-string-literal */
+        /* tslint:disable:semicolon */
+        /* tslint:disable:no-unused-expression */
+        // This code is from Google, so let's not modify it too much, just add gaNewElem and gaElems:
+
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*currdate;a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga', gaNewElem, gaElems);
+        
+        /* tslint:enable:no-unused-expression */
+        /* tslint:enable:semicolon */
+        /* tslint:enable:no-string-literal */
+        ga('create', 'UA-59148878-1', 'auto');
+        GAService.gaCreated = true;
+      }
   }
 
   public static event(eventCategory:string, eventAction:string, eventLabel:string='', eventValue:number=0){
-    // GAService.spawn();
-    // ga('send', 'event', eventCategory, eventAction, eventLabel, eventValue);
+    GAService.spawn();
+    ga('send', 'event', eventCategory, eventAction, eventLabel, eventValue);
   }
 
   public static screenview(screenName:string){
 
-    // if(GAService.appName == undefined || GAService.appVersion == undefined){
-    //     throw "App not configured";
-    // }
+    if(GAService.appName == undefined || GAService.appVersion == undefined){
+        throw "App not configured";
+    }
 
-    // GAService.spawn();
-    // ga('send', 'screenview', {
-    //   'appName': GAService.appName,
-    //   'appVersion': GAService.appVersion,
-    //   'screenName': screenName
-    // });
+    GAService.spawn();
+    ga('send', 'screenview', {
+      'appName': GAService.appName,
+      'appVersion': GAService.appVersion,
+      'screenName': screenName
+    });
   }
 
   public static pageview(page:string){
-    // GAService.spawn();
-    // ga('set', 'page', page);
-    // ga('send', 'pageview')
+    GAService.spawn();
+    ga('set', 'page', page);
+    ga('send', 'pageview')
   }
 
 

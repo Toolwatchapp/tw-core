f256e588bd7d8b5c48a5ab6bd9f7cef78d74fef6
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2017-04-30 18:52:16 -0400

    test(coverage): Fail if coverage isn't 100%

---
+++ coverage-checker.sh
@@ -0,0 +1,9 @@
+count=$(grep "<span class=\"strong\">100% </span>" coverage/html/index.html | wc -l);
+
+if [ "$count" -ne 3 ] 
+then
+    echo "Coverage isn't 100%"
+    exit 1
+else
+    echo "Coverage is 100%"
+fi

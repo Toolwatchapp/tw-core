1c4bae0444e5c340e537b02fee9c6463b3732c96
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2017-05-17 12:38:55 -0400

    refactor(bundle): export classes for external usage

--- index.ts
+++ index.ts
@@ -14,6 +14,33 @@ import { SignupComponentModule } from './src/directives/signup/signup.component'
 import { LoginComponentModule } from './src/directives/login/login.component';
 import { ClockComponentModule } from './src/directives/clock/clock.component';
 
+/**
+ * SERVICES
+ */
+export * from './src/services/twapi.service'
+export * from './src/services/analytics.service'
+export * from './src/services/configuration.service'
+/**
+ * PIPES
+ */
+export * from './src/pipes/arethmetic-sign.pipe'
+export * from './src/pipes/leading-zero.pipe'
+export * from './src/pipes/k-formatter.pipe'
+/**
+ * MODEL
+ */
+export * from './src/models/watch.model'
+export * from './src/models/user.model'
+export * from './src/models/measure.model'
+export * from './src/models/blog-post.model'
+/**
+ * DIRECTIVES
+ */
+export * from './src/directives/clock/clock.component'
+export * from './src/directives/clock/moon-phases/moon-phases.component'
+export * from './src/directives/login/login.component'
+export * from './src/directives/signup/signup.component'
+export * from './src/directives/watch/watch.component'
 
 @NgModule({
   declarations: [],
